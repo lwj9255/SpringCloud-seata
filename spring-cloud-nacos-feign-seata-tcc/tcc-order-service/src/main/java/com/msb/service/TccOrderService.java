7cf5fe9b742ec4319244686bd870fa535fcd1279
lwj9255 <lwj1059168506@gmail.com>
2025-02-12 15:17:42 +0800

    2.12 seata结束

--- spring-cloud-nacos-feign-seata-tcc/tcc-order-service/src/main/java/com/msb/service/TccOrderService.java
+++ spring-cloud-nacos-feign-seata-tcc/tcc-order-service/src/main/java/com/msb/service/TccOrderService.java
@@ -5,6 +5,7 @@ import io.seata.rm.tcc.api.BusinessActionContext;
 import io.seata.rm.tcc.api.BusinessActionContextParameter;
 import io.seata.rm.tcc.api.LocalTCC;
 import io.seata.rm.tcc.api.TwoPhaseBusinessAction;
+
 @LocalTCC
 public interface TccOrderService {
 
